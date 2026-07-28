ee5a96900d77f6ccf39350dd4ab4d76bf364497e
filipdkolodziejczyk <filipdkolodziejczyk0@gmail.com>
2026-07-28 14:06:01 +0100

    v2.0 MatrixLUT encoder logic and debounce counter
    - Matrix look up table improved readability of what is happening
    - Encoder noise was eliminated using the debounce counter
    - Distance tracking is accurate and precise for any selected distance

--- MovingForwardOnlyMay/MovingForwardOnlyMay.ino
+++ MovingForwardOnlyMay/MovingForwardOnlyMay.ino
@@ -10,28 +10,48 @@
 
 const int ledChannela = 0;
 const int ledChannelb = 1;
-//gpio pins used for left motor
+
+// GPIO pins used for left motor
 const int leftmotorChannelaPin = 36;
 const int leftmotorChannelbPin = 39;
 
-int i = 0;
 int leftSignalA = 0;
 int leftSignalB = 0;
 int pulseCount = 0;
 
-//enum assigns variables to numbers but it is always starts with 0 then 1 so on..
+
+const float singlePulseDist = ( M_PI * 3.32) / 36;       
+
+// Input distance wanted into this variable IN CM
+const int InputDist = 500;
+
+
+int NumOfPulses = (int)(InputDist / singlePulseDist);   
+
+// State mapping variables
 enum States {S0 = 0, S1 = 1, S2 = 2, S3 = 3};
 States currentState;
-//define single pulse distance IN CM
-const float singlePulseDist = (M_PI*3.1)/5;
-//Input distance wanted into this variable IN CM
-const int InputDist = 38;
+
+const int encoderTable[4][4]={
+    /*New states      00,  01, 10, 11*/
+    /*old state 00*/{  0,  0,  1,   -1},
+    /*old state 01*/{  0,  0,  0,   0 },
+    /*old state 10*/{ -1,  0,  0,   1 },
+    /*old state 11*/{  1,  0, -1,   0 }       
+};                
+
+//Debounce filter 
+int stableStateNum = 0;
+int debounceCounter = 0;
+const int debounceThreshold = 6;     // i need a consecutive 15 of the same state to use the matix table
+
+int currentStateNum;
+int previousStateNum;
 
 void setup() {
-
-  //conffiguring encoder pins as inputs
-  pinMode(leftmotorChannelaPin,INPUT);
-  pinMode(leftmotorChannelbPin,INPUT);
+  // Configuring encoder pins as inputs with pullups active
+  pinMode(leftmotorChannelaPin, INPUT_PULLUP);
+  pinMode(leftmotorChannelbPin, INPUT_PULLUP);
   
   // Configure motor direction pins as outputs
   pinMode(INa, OUTPUT);
@@ -43,148 +63,108 @@ void setup() {
   ledcAttachPin(enA, ledChannela);
   ledcAttachPin(enB, ledChannelb);
 
-  // Initialize PWM channels
-  ledcSetup(ledChannela, 1000, 8); // 1000 Hz PWM, 8-bit resolution
-  ledcSetup(ledChannelb, 1000, 8); // 1000 Hz PWM, 8-bit resolution
+  // Initialize PWM channels (1000 Hz PWM, 8-bit resolution)
+  ledcSetup(ledChannela, 1000, 8); 
+  ledcSetup(ledChannelb, 1000, 8); 
 
-  //define a cuurent state depending on real position of encoder and also reaidng it just once before it starts 
+  // Read initial startup position of encoder
   leftSignalA = digitalRead(leftmotorChannelaPin);
   leftSignalB = digitalRead(leftmotorChannelbPin);
-  if(leftSignalA == 0 && leftSignalB == 0){
-    currentState = S0;
-  }
-  else if(leftSignalA == 1 && leftSignalB == 0){
-    currentState = S1;
-  }
-  else if(leftSignalA == 1 && leftSignalB == 1){
-    currentState = S2;
-  }
-else{
-  currentState = S3;
-}
+  
+  if(leftSignalA == 0 && leftSignalB == 0)      { currentState = S0; previousStateNum = 0; }
+  else if(leftSignalA == 1 && leftSignalB == 0) { currentState = S1; previousStateNum = 2; }         
+  else if(leftSignalA == 1 && leftSignalB == 1) { currentState = S2; previousStateNum = 3; }         
+  else                                          { currentState = S3; previousStateNum = 1; }          
+
+  //initialises what the state is at the start for the debounce filter
+  stableStateNum = previousStateNum;
+
   // Begin serial communication
   Serial.begin(115200);
-  Serial.println("ESP32 Running"); 
+  Serial.println("ESP32 Running..."); 
+  Serial.print("Target Pulses to reach: ");
+  Serial.println(NumOfPulses);
 }
 
 void loop() {
+  //  Read pins instantly
   leftSignalA = digitalRead(leftmotorChannelaPin);
   leftSignalB = digitalRead(leftmotorChannelbPin);
-  //Serial.println(singlePulseDist);
 
-  //Serial.print("LeftMotor Channel A : ");
-  //Serial.println(leftSignalA);
+  //  Map pins to matrix state index
+  currentStateNum = (leftSignalA << 1) | leftSignalB;             
 
-  //Serial.print("LeftMotor Channel B : ");
-  //Serial.println(leftSignalB);
-  LeftEncoderPulseCount(leftSignalA,leftSignalB);
+  //  Process the encoder changes
+  LeftEncoderPulseCount(leftSignalA, leftSignalB);
 
-  goForwards();
-  // Add a delay or other functions to control when directions change or stop
-  DistanceCalculator (pulseCount);
-
-  
+  //  Evaluate if target is reached
+  DistanceCalculator(pulseCount);
 }
 
 void goForwards() {
-
-
   // Set motor direction for forwards movement
   digitalWrite(INa, LOW);
   digitalWrite(INb, HIGH);
   digitalWrite(INc, HIGH);
   digitalWrite(INd, LOW);
 
-
-  // Set speed (duty cycle) for both motors
-  ledcWrite(ledChannela, 90); // Full speed for motor A
-  ledcWrite(ledChannelb, 90); // Full speed for motor B
-
-  //delay(1000); // Example delay - adjust as needed for your application
-
-  // You might want to add code here to stop the motors after moving forward
+  // Drive at speed 90
+  ledcWrite(ledChannela, 90); 
+  ledcWrite(ledChannelb, 90); 
 }
 
 void Stop() {
-  // Set motor direction for forwards movement
+  // 
   digitalWrite(INa, HIGH);
-  digitalWrite(INb, LOW);
-  digitalWrite(INc, LOW);
+  digitalWrite(INb, HIGH);
+  digitalWrite(INc, HIGH);
   digitalWrite(INd, HIGH);
-  // Set speed (duty cycle) for both motors
-  ledcWrite(ledChannela, 100); // Full speed for motor A
-  ledcWrite(ledChannelb, 100); // Full speed for motor B
-  delay(60);
-  digitalWrite(INa, HIGH);
-  digitalWrite(INb, LOW);
-  digitalWrite(INc, LOW);
-  digitalWrite(INd, HIGH);
-  // Set speed (duty cycle) for both motors
-  ledcWrite(ledChannela, 0); // Full speed for motor A
-  ledcWrite(ledChannelb, 0); // Full speed for motor B
-  delay(100000000);
-  Serial.print("Distance travelled/cm : ");
-  Serial.println(InputDist);
+
+  // Kill power completely
+  ledcWrite(ledChannela, 0); 
+  ledcWrite(ledChannelb, 0); 
+  
+  // Print final resting position 
+  static bool printed = false;
+  if(!printed) {
+   // Serial.print("Target Reached! Final Pulse Count: ");
+    Serial.println(pulseCount);
+    printed = true;
+  }
 }
 
-void DistanceCalculator (int pulseCount){
-  int NumOfPulses = (InputDist-10)/singlePulseDist;       //19cm is the distance from the back to the front wheels to take into account that the back wheels will be set back 
-  if(pulseCount>NumOfPulses){
+void DistanceCalculator(int currentPulses){      
+  if(currentPulses < NumOfPulses){
+    goForwards();
+  }
+  else{
     Stop();
   }
 }
 
-int LeftEncoderPulseCount (int leftSignaA, int leftSignalB){
-
-      switch(currentState){
-        case S0:                                    //currently at S0
-         if(leftSignalA == 1 && leftSignalB == 0){
-           currentState = S1;                          // MOVE TO NEXT STATE
-          pulseCount ++;
-          Serial.print("Pulse count: ");                              //mealy machine output
-          Serial.println(pulseCount);                 // printing mealy machine output
-         }
-       else if(leftSignalA == 0 && leftSignalB == 1) {
-        currentState = S3;                            //backwards movement
-       }
-       break;
+int LeftEncoderPulseCount(int leftSignaA, int leftSignalB){
+  
+  // If the pins match our accepted stable state, reset the counter
+  if (currentStateNum == stableStateNum) {
+    debounceCounter = 0;
+  } 
+  // If the pins changed, start counting how long they stay at this new value
+  else {
+    debounceCounter++;
     
-        case S1:
-        if(leftSignalA == 1 && leftSignalB == 1){
-          currentState = S2;
-          pulseCount ++;
-          Serial.print("Pulse count: ");
-          Serial.println(pulseCount); 
-        }
-        else if(leftSignalA == 0 && leftSignalB ==0){
-          currentState = S0;                        // backwards movement
-        }
-        break;
+    //  Only accept the state change if it clears the threshold without flickering
+    if (debounceCounter >= debounceThreshold) {
+      int MatrixValue = encoderTable[stableStateNum][currentStateNum];
+      pulseCount += MatrixValue;
       
-        case S2:
-        if(leftSignalA == 0 && leftSignalB == 1){
-          currentState = S3;
-          pulseCount ++;
-          Serial.print("Pulse count: ");
-          Serial.println(pulseCount); 
-        }
-        else if(leftSignalA == 1 && leftSignalB == 0)
-        currentState = S1;                          // backward movement
-        break;
-
-        case S3:
-        if(leftSignalA == 0 && leftSignalB == 0){
-          currentState = S0;
-          pulseCount ++;
-          Serial.print("Pulse count: ");
-          Serial.println(pulseCount);
-        }
-        else if (leftSignalA == 1 && leftSignalB == 1){
-          currentState = S2;                        //backwards
-        }
-        break;
-      }
-      return pulseCount;
+      //Serial.print("Live Count: ");
+      Serial.println(pulseCount);
+      
+      // Update our memories to the new confirmed state
+      stableStateNum = currentStateNum;
+      previousStateNum = currentStateNum;
+      debounceCounter = 0;
+    }
+  }
+  return pulseCount;
 }
-      
-
